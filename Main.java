96c91b0fc9368b5b5883bc7dbe2e12df23775fb4
Aaniyah <aaniyah.ellis2027@gbwl.org>
2025-11-07 13:31:26 +0000

    2.4 notes

--- Main.java
+++ Main.java
@@ -69,6 +69,24 @@ public class Main {
       else {
          System.out.println("Flipped tails Wear Black!");
       }
+      
+      // MULTI-WAY SELECTION
+      //1. NESTING if statements if like asking "follow-up questions"
+      int age = 16;
+      boolean havePermit = false;
+      System.out.println("Let's see if you can take this road test...");
+      if (age >= 16) {
+         System.out.println("Alright, I guess you're old enough...");
+         //After passing the first condition (age>=16)
+         // you ask a FOLLOW-UP (another if statement)
+         if ( havePermit == true){
+            System.out.println("Fine, get in the car");
+         }
+      }
+      else {
+         System.out.println("you're too young, womp womp");
+      }
+
 
 
    }
